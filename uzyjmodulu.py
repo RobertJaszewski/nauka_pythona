7f7b798ca0ccdfc41357e83b044377e7382a73ac
RobertJaszewski <RobertJaszewski@users.noreply.github.com>
2022-05-29 10:41:38 +0200

    Added uzyjmodulu.py

---
+++ uzyjmodulu.py
@@ -0,0 +1,10 @@
+# importujemy moduł i nadajemy mu alias
+from mytoolkit import matematyczny as mat
+from mytoolkit import pomocniczy as pom #j.w tylko dla drugiego moduły
+
+# wywołanie funkcji/metod z mytoolkit/matematyczny (alias: 'mat')
+print(mat.dodaj(4,14))
+print(mat.odejmij(10, 5))
+
+# wywołanie funkcji/metody z mytoolkit/pomocniczy (alias: 'pom')
+pom.czesc()
