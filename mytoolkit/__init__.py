248c623303dd48df1192219030bb52fac737e218
RobertJaszewski <RobertJaszewski@users.noreply.github.com>
2022-05-29 10:38:43 +0200

    Added files with modules

---
+++ mytoolkit/__init__.py
@@ -0,0 +1,4 @@
+# Plik o tej nazwie wgrany do folderu mytoolkit informue środowisko Pythona, że cały katalog jest
+# Pakietem
+# Zmienna all zawiera listę modułów zawartych w pakiecie
+_all__ = ["matematyczny", "pomocniczy"]
---
+++ mytoolkit/pomocniczy.py
@@ -0,0 +1,4 @@
+# Stworzymy metode bezparametrową, która nie będzie zwracała wartości
+def czesc():
+    #Blok należący do funkcji 
+    print("Wywołałeś mnie z modułu pomocniczy")
